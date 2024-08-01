20632d9548405fc6e1eb9424d95d87e354a1829a
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-08-01 17:48:24 -0400

    I got it mostly working!

--- Code/backend/main.py
+++ Code/backend/main.py
@@ -4,6 +4,7 @@ from flask import Flask, request
 from flask_cors import CORS
 import os
 import json
+import pandas as pd
 
 cwd = os.getcwd()
 
@@ -34,10 +35,40 @@ def get_data():
     devices = received["devices"]
     devices = devices.replace(" ", "")
     devices = devices.split(",")
+    print(devices)
 
+    jsonPacket = []
 
-    return received, 200
+    for device in devices:
+        device = int(device)
+        path = f"{cwd}/data/{device}.csv"
+        if os.path.exists(path):
+            df = pd.read_csv(path)
+            latest_data = df.iloc[-1]
+            packet = {
+                "device": int(latest_data[1]),
+                "soilHumidity": int(latest_data[2]),
+                "airHumidity": int(latest_data[3]),
+                "temperature": float(latest_data[4]),
+                "pressure": float(latest_data[5]),
+            }
+            jsonPacket.append(packet)
+
+    json_response = json.dumps(jsonPacket)
+    print(json_response)
+    return json_response, 200
 
 
 if __name__ == "__main__":
     app.run(debug=True, port=7359)
+
+    #  with open(path, "r") as f:
+    #             data = json.load(f)
+    #             key = data[str(len(data))]
+    #             print(key)
+    #         data[key].append(f"device: {device}")
+    #         print(data[key])
+    #         with open(path, "w") as f:
+    #             json.dump(data, f, indent=4)
+    #     else:
+    #         return 404
