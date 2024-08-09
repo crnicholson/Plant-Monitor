3d7d9532f124ac423aee4517560b2b620e73d93d
Charles Nicholson <83499056+crnicholson@users.noreply.github.com>
2024-08-09 15:25:10 -0400

    PCB stuff, file org

--- Code/frontendArchive/templates/device.tsx
+++
@@ -1,9 +0,0 @@
-export interface Device {
-    alias: string;
-    device: number;
-    soilHumidity: number;
-    airHumidity: number;
-    temperature: number;
-    pressure: number;
-    time: string;
-}
